1bd3481c2b8d73701e6edc1d2dc391e97701d9e8
RafrafiAhmed <ahmedrafrafi25@gmail.com>
2020-12-21 14:34:14 +0100

    Ahmed-2

--- baftekhome/frontEnd/src/app/host/host-profile/host-profile.component.ts
+++ baftekhome/frontEnd/src/app/host/host-profile/host-profile.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-host-profile',
-  templateUrl: './host-profile.component.html',
-  styleUrls: ['./host-profile.component.scss']
+  selector: "app-host-profile",
+  templateUrl: "./host-profile.component.html",
+  styleUrls: ["./host-profile.component.scss"]
 })
 export class HostProfileComponent implements OnInit {
+  profile: any = {
+    userName: "aa",
+    email: "aa",
+    firstName: "aa",
+    lastName: "aa",
+    password: "aa",
+    image:
+      "https://i.kinja-img.com/gawker-media/image/upload/t_original/ijsi5fzb1nbkbhxa2gc1.png",
+    adress: "aa",
+    rate: "aa"
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
