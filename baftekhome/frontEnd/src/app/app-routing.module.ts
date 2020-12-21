f0f935f5252d91187a99cccd12643795f1379214
DhiaDhafer <dhiadhaferr@gmail.com>
2020-12-21 21:31:07 +0100

    mergin master from the org

--- baftekhome/frontEnd/src/app/app-routing.module.ts
+++ baftekhome/frontEnd/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { LoginComponent } from './login/login.component';
 import { GuestSignUpComponent } from './guest/guest-sign-up/guest-sign-up.component';
 import { HostSignUpComponent } from './host/host-sign-up/host-sign-up.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
+import { GuestSinglePostViewComponent} from './guest/guest-single-post-view/guest-single-post-view.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: 'HostSignUp', component: HostSignUpComponent},
-  {path: 'GuestSignUp', component: GuestSignUpComponent}
+  {path: 'GuestSignUp', component: GuestSignUpComponent},
+  {path: 'GuestSinglePostView', component: GuestSinglePostViewComponent}
 ];
 
 @NgModule({
